15d85b8b782c8dfc14545db1b43336e46dd089d9
julydev144hz <ippolito.julianet36@gmail.com>
2022-09-23 08:44:36 -0300

    terminado

---
+++ poo_git/src/poo_git/Manzana.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo_git;
+
+public class Manzana extends Producto{
+    private int kg;
+    private double precio;
+
+    public Manzana(int kg, double pcio, int cod, String desc) {
+        super(cod, desc, pcio);
+        this.kg = kg;
+        this.precio = precio;
+    }
+
+    public String toString() {
+        return "Manzana "+this.kg+"kg : "+"$"+this.calcularTotal();
+    }
+    
+    public double calcularTotal(){
+        
+        this.precio += this.precio * this.kg;
+        
+        return this.precio;
+    }
+    
+    
+}
