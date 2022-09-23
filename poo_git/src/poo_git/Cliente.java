17feda19ad9b4b1d3701fbf85406b34e25029ffb
ElGuily <martinez.francoet36@gmail.com>
2022-09-23 08:21:19 -0300

    clase Cliente

---
+++ poo_git/src/poo_git/Cliente.java
@@ -0,0 +1,17 @@
+package poo_git;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ET36
+ */
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private int DNI = 0;
+    private 
+}
